dabb0241861d116a621de377fc70ac77f666ffc9
atanazy-tot <kmjablonka@gmail.com>
2024-06-02 20:38:27 +0200

    added input_digester to grip data from selected section; added analysis for our output (testing is needed); few changes in function and variable naming

--- analysis_test.py
+++ analysis_test.py
@@ -1,2 +1,5 @@
-from src import check
+from src.neos_output_parser import get_variables_from_neos
 
+
+variables = get_variables_from_neos('results/archiv/neos_outputs/neos_output_2.txt')
+print(variables)
--- main.py
+++ main.py
@@ -3,12 +3,12 @@ Main file
 """
 
 from src.input_parser import generate_dat, employer_demands
-from src.check_output_sense import check, timetable
+from src.neos_output_parser import get_variables_from_neos, create_timetable
 
 input_path = 'data/test_instance.csv'
 output_path = 'neos_output_2_variant1.txt'
 N, J, K = generate_dat(input_path, 'model/dat_files/first_data.dat')
 time, empl_demands = employer_demands(input_path, J, K)
 
-Variables = check(output_path)
-timetable(Variables, N, J, K, 'results/archiv/timetable_neos_output_2_variant1.csv', empl_demands)
+Variables = get_variables_from_neos(output_path)
+create_timetable(Variables, N, J, K, 'results/archiv/timetable_neos_output_2_variant1.csv', empl_demands)
