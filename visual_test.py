3d02e3054291418fc4964e2db6661bab0dcd2edf
mskrool <mskrool@gmail.com>
2024-06-04 20:39:03 +0200

    add condition no. 7 to model and run test_instance plus minor fixes

--- visual_test.py
+++ visual_test.py
@@ -1,19 +1,18 @@
-from parse_vacation import parse_vacation
+from src.visualisation.parse_vacation import parse_vacation
 from src.visualisation.schedule_graph import plot_week_schedule, plot_nurse_schedule
 import pandas as pd
 
 # Read the data into a DataFrame
-df = pd.read_csv("results/archiv/timetable_neos_output_2_variant1.csv", sep=';', header=None)
+# Data has to come as output from neos_output_parser.py
+df = pd.read_csv("results/archiv/timetables/test_timetable.csv", sep=';', header=None)
 df = df.iloc[2:]
 df = df.reset_index(drop=True)
 df = df.set_index(df.columns[0])
 df = df.fillna(0).astype(int)
-nurses_total_hours = df.iloc[:-2, -1].tolist()
-df = df.iloc[:, :-1]
 column_names = [f"Day_{k}_Shift_{j}" for k in range(1, 29) for j in range(1, 4)]
 df.columns = column_names
 
-vacation_data = parse_vacation('data/test_instance.csv')
+vacation_data = parse_vacation('data/test_instance_last_shift.csv')
 vacation_data.columns = column_names
 
 
@@ -22,5 +21,5 @@ for week in range(1, 5):
     plot_week_schedule(df, week, vacation_data)
 
 # Plot schedule for individual nurse
-nurse_id = 4
+nurse_id = 11
 plot_nurse_schedule(df, nurse_id, vacation_data)
